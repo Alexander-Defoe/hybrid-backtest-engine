027e4a1955fceead499b0d730780f697db9e92a5
ad887 <ad887@kent.ac.uk>
2026-03-20 19:43:51 +0000

    Updated main.py to print the SMA

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ symbols = ['AAPL', 'MSFT', 'GOOGL', 'AMZN']
 # Calls the modular function, passes in the symbols and saves the result in a 2D matrix
 close_matrix = getPriceData.get_price_data(symbols)
 
-# Hands the matrix to C++ for multithreaded processing
-averages = my_cpp_module.calculate_averages(close_matrix)
-print("Averages calculated by C++:", averages)
+sma_window = 20
+
+sma_results = my_cpp_module.calculate_smas(close_matrix, sma_window)
+
+print("SMA Matrix Shape:", sma_results.shape)
+
+print("\nResults:")
+print(sma_results)
