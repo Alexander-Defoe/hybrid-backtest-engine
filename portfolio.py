647f20a490f731807fefae3532cc31c0f329deb3
ad887 <ad887@kent.ac.uk>
2026-03-27 15:49:42 +0000

    Added stop-loss and performance metrics
    Implemented a 5% stop-loss in the Portfolio class and added Sharpe Ratio and Max Drawdown calculations to measure risk v reward

--- portfolio.py
+++ portfolio.py
@@ -5,9 +5,21 @@ class Portfolio:
         self.positions = {ticker: 0 for ticker in ticker_list}
         # List to track the total account value over time
         self.equity_curve = []
+        # Dictionary to track the prices paid for each stock
+        self.buy_prices = {ticker: 0.0 for ticker in ticker_list}
 
     def handle_signal(self, ticker, current_price, signal):
         commission_rate = 0.001
+        # Maximum loss percentage permitted
+        stop_loss_percentage = 0.05
+
+        # If a stock is owned it checks if a stop-loss needs to be triggered
+        if self.positions[ticker] > 0:
+            loss = self.buy_prices[ticker] * (1.0 - stop_loss_percentage)
+            # If the price drops below the stop_loss_percentage a sell signal is forced
+            if current_price <= loss:
+                signal = -1.0
+                print(f"Stop Loss triggered for {ticker} at {current_price}")
         # Buys as many shares as the total cash can afford
         if signal == 1.0 and self.cash >= current_price:
             shares_to_buy = self.cash // current_price
@@ -18,6 +30,8 @@ class Portfolio:
             if self.cash >= real_cost:
                 self.positions[ticker] += shares_to_buy
                 self.cash -= real_cost
+                # Sets the new buy price
+                self.buy_prices[ticker] = current_price
 
         # Sells all shares for this stock
         elif signal == -1.0 and self.positions[ticker] > 0:
@@ -27,6 +41,7 @@ class Portfolio:
             
             self.positions[ticker] = 0
             self.cash += real_payout
+            self.buy_prices[ticker] = 0.0
 
 # Calculates the total account value for the current day
     def update_equity(self, current_prices):
